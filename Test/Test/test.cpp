34959393a8d407aa4756b1a23865fc0ea471ca66
nan1492 <46950830+nan1492@users.noreply.github.com>
2023-02-10 23:55:22 +0900

    first

---
+++ Test/Test/test.cpp
@@ -0,0 +1,44 @@
+#include <iostream> 
+using namespace std;
+
+int main()
+{
+	bool bbb = true;
+
+	bbb |= false;
+	bbb |= true;
+
+
+	long l = 0;
+	long l1 = 1;
+	long l2 = 2;
+	long l3 = 3;
+
+	if (false && false)
+	{
+		int aaa = 1;
+	}
+
+	printf_s("%d\n", l | l1);
+	printf_s("%d\n", l | l2);
+	printf_s("%d\n", l | l3);
+
+	l |= l2;
+	l |= l2;
+	l |= l2;
+	l |= l2;
+	l |= l2;
+	l |= l2;
+	printf_s("%d\n", l);
+
+	l |= l1;
+	l |= l1;
+	l |= l2;
+	l |= l2;
+	l |= l2;
+	l |= l2;
+	printf_s("%d\n", l);
+
+	//¾Æ ½Ã¹ß
+
+}
